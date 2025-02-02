7464e9cf13d54fcaffd0d0f4c1525598a108cc43
Asaduzzaman-Programmer <asaduzzamanprogrammer@gmail.com>
2025-02-02 20:12:36 +0600

    Design with responsive

--- src/components/gadget/Gadget.jsx
+++ src/components/gadget/Gadget.jsx
@@ -1,33 +1,30 @@
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-
-
-import React from 'react';
-import PropTypes from 'prop-types';
-
-const Gadget = ({gadget}) => {
-    const {product_id, product_title, price, product_image} = gadget
-    return (
-        <div className="card bg-base-100 w-85 shadow-xl mr-20 mb-7 ">
-        <figure className="px-10 pt-10">
-          <img
-           
-            src={product_image}
-            alt="Shoes"
-            className="rounded-xl h-50" />
-        </figure>
-        <div className="card-body ">
-          <h2 className="card-title font-bold text-gray-600">{product_title}</h2>
-          <p className='font-bold text-gray-600'>{price}</p>
-          <div className="card-actions">
-          <button className="btn btn-outline btn-primary rounded-2xl">View Details</button>
-          </div>
+const Gadget = ({ gadget }) => {
+  const { product_title, price, product_image } = gadget;
+  return (
+    <div className="card bg-base-100 w-85 shadow-xl mr-20 mb-7 ">
+      <figure className="px-10 pt-10">
+        <img src={product_image} alt="Shoes" className="rounded-xl h-50" />
+      </figure>
+      <div className="card-body ">
+        <h2 className="card-title font-bold text-gray-600">{product_title}</h2>
+        <p className="font-bold text-gray-600">{price}</p>
+        <div className="card-actions">
+          <Link>
+            <button className="btn btn-outline btn-primary rounded-2xl">
+              View Details
+            </button>
+          </Link>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 Gadget.propTypes = {
-    
+  gadget: PropTypes.any,
 };
 
 export default Gadget;
--- src/statistic/Statistic.jsx
+++ src/statistic/Statistic.jsx
@@ -1,11 +1,9 @@
-import React from 'react';
-
 const Statistic = () => {
-    return (
-        <div className="px-8">
-            <h1>I am statistic</h1>
-        </div>
-    );
+  return (
+    <div className="px-8">
+      <h1>I am statistic</h1>
+    </div>
+  );
 };
 
 export default Statistic;
